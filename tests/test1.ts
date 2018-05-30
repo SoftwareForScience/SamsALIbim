ff8d33c753ce1cc9e600c99e890c0a6a25218c53
Your Name <you@example.com>
2018-05-30 13:36:05 +0200

    Fixed test

--- tests/test1.ts
+++ tests/test1.ts
@@ -4,7 +4,7 @@ import { SamsSettings } from '../src/SamsSettings';
 
 describe('Hello function', () => {
     it('should return hello world', () => {
-      const mySams = new Sams(new SamsSettings("", "", 1));
+      const mySams = new Sams(new SamsSettings("token", "url", 1, 1, "user", "name"), null);
     //  expect(result).to.equal('Hello World!');
     });
   });
